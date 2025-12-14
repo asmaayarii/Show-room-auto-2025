0a0fa9d61c3a499b00be6bb2fefa1e7e58fc31b5
Asmaa Ayari <asma.ayari.2023@ihec.ucar.tn>
2025-12-14 22:53:24 +0100

    Initial commit

---
+++ src/app/components/search-bar/search-bar.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Auto } from '../../Interfaces/auto';
+import { CurrencyPipe, NgClass, NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [CurrencyPipe, NgClass , NgStyle],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.css'
+})
+export class SearchBar {
+
+  @Input() autos: Auto[] = [];
+  @Output() onSelectAuto = new EventEmitter<Auto>();
+  selectedAutos: Auto[] = [];
+
+  selectAutoList(brand: string) {
+    this.selectedAutos = this.autos.filter(x=>x.brand.toLowerCase().startsWith(brand.toLowerCase()));
+    
+  }
+
+showDetails(auto: Auto) {
+  this.onSelectAuto.emit(auto)
+}   
+autoTitleStyle(auto:Auto){
+  if(auto.power>=10)
+    return {'color':'red'}
+  else
+    return {'color':'black'}
+}
+}
